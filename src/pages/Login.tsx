1aa1bd957fc9e0d37464935e5d11fa90e4469025
5935512036 <5935512036@psu.ac.th>
2020-12-02 12:50:43 +0700

    first commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components"
+
+import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/react';
+
+const StyledWrapper = styled.div`
+    background-image: url('https://lh3.googleusercontent.com/lkna-rrOEjpKNmNcMrKrKXIjqJ-Tyi5jBp0o7tGb5k75JCk9fbFlD5rPLLIrtknUG9yRF-Y2GCsNIZMy=w1080-h608-p-no-v0');
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+`
+
+const Login = () => {
+    return (
+        <StyledWrapper>
+           
+        </StyledWrapper>
+    )
+}
+
+export default Login
