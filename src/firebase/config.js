4c8748bfc13583e74e0683bdcaa7001ff193b937
Madsoffee YK <sopee_namyen@hotmail.com>
2020-12-03 15:02:23 +0700

    master

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBpVtAMxcj8svVuembrLV2fCm5urXXYwgE",
+    authDomain: "repair-notification-74343.firebaseapp.com",
+    databaseURL: "https://repair-notification-74343.firebaseio.com",
+    projectId: "repair-notification-74343",
+    storageBucket: "repair-notification-74343.appspot.com",
+    messagingSenderId: "569017176575",
+    appId: "1:569017176575:web:32de2c34c4d6d98ad197ca",
+    measurementId: "G-NY29SVTVN4"
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from "styled-components"
-
 import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/react';
-
+// import firebase from '../firebase'
+import firebase from 'firebase'
 const StyledWrapper = styled.div`
     background-image: url('https://lh3.googleusercontent.com/lkna-rrOEjpKNmNcMrKrKXIjqJ-Tyi5jBp0o7tGb5k75JCk9fbFlD5rPLLIrtknUG9yRF-Y2GCsNIZMy=w1080-h608-p-no-v0');
     height: 100%;
@@ -11,9 +11,16 @@ const StyledWrapper = styled.div`
 `
 
 const Login = () => {
+    const test = firebase.firestore().collection('repairs').doc('riMMYqyHRSJThfFUwKqU')
+        .get()
+        .then((doc) => {
+            console.log(doc.data());
+        })
+    console.log(test);
+
     return (
         <StyledWrapper>
-           a
+
         </StyledWrapper>
     )
 }
